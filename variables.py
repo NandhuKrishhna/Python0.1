d51c83f0399a37814f01f793257ca9d0cb26eb81
NandhuKrishhna <nandhukrishna393@gmail.com>
2025-08-23 05:35:00 +0530

    commnets

--- variables.py
+++ variables.py
@@ -1,11 +1,6 @@
-def sum_all(*args):
-    return sum(args)
-print(sum_all(1,2,3))  # 6
-
-def print_info(**kwargs):
-    for k,v in kwargs.items():
-        print(k,v)
-print_info(name="Alice", age=25)
-
-name = "Nandhu";
-print(name.split())
+Apple = 'Apple';
+apple = 'apple'
+print(Apple)
+print(apple)
+print(id(apple))
+print(id(Apple))
